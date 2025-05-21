da041e10282383383af769a658a86db611572a61
Jaxon Adams <jadamsresume1@gmail.com>
2025-05-20 20:51:50 -0600

    Bootstrap CDK project

---
+++ test/blog-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlogBackend from '../lib/blog-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blog-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlogBackend.BlogBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
