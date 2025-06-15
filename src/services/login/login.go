7847aaa408bf14267ef29dc85b535e94c218c00f
Jaxon Adams <jadamsresume1@gmail.com>
2025-06-15 15:46:57 -0600

    Outline next steps for login flow

--- src/services/login/login.go
+++ src/services/login/login.go
@@ -7,5 +7,10 @@ import (
 )
 
 func LogInAdmin(input models.AdminLoginInput, services models.HandlerServices, ctx context.Context) error {
+
+	// Fetch the stored password hash from dynamodb
+	// Compare the input password to the fetched hash
+	// If correct, generate and return a new JWT
+
 	return nil
 }
